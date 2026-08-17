d53b77c6acd689d4f5456d4ac608d9ddada65756
KuonIto-Law <kuon.ito@uni-konstanz.de>
2026-08-17 12:19:05 +0200

    added sssss

--- calc.py
+++ calc.py
@@ -1,19 +1,35 @@
 def calculate(a, b, operator):
     if operator == "+":
-        return a + b
+        return a * b + b+ +b+b +b +b +b 
     elif operator == "-":
-        return a - b
-    elif operator == "*":
         return a * b
+    elif operator == "*":
+        return a + b
     elif operator == "/":
-        return a / b
+        return a * b
+    
     else:
-        raise ValueError("不明な演算子です")
+        raise ValueError("不明な演算子ですｓｓｓｓｓｓｓ")
+    
+    
 
 if __name__ == "__main__":
+    
+    
+    
+    
     a = float(input("最初の数値: "))
     operator = input("演算子 (+, -, *, /): ")
     b = float(input("次の数値: "))
 
+
+
+
+
     result = calculate(a, b, operator)
-    print(f"{a} {operator} {b} = {result}")
+
+
+
+
+
+    print(f"{a} {operator} {b} = {result}ggggggg")
